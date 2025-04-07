3ca724fb505dab4be57493cd8e46897e310a17aa
Nico Linux Desktop <NicoloRomito@protonmail.com>
2025-04-07 12:31:37 +0200

    [UPDATE]: Check the TODO's to try the first API calls to backend. [UPDATE]: Start to do the search page in order to filter by vegetables, fruit and alcohol for now, can be added later. [TODO]: Finish the Home button. [TODO]: Finish the profile button with authentication and registration.

--- projects/shared/src/lib/services/search-bar.service.ts
+++ projects/shared/src/lib/services/search-bar.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchBarService {
+  // TODO: change the apiUrl with call to backend
 
-  constructor() { }
+  private apiBackendUrl = 'http://localhost:4201/api/cocktails?name=';
+  private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+  constructor(private http: HttpClient) {}
+
+  searchEngine(query: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${query}`);
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SearchBarComponent } from '../../../projects/shared/src/public-api';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  searchQuery: string = '';
+  noResultsFound: boolean = false;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.searchQuery = params['query'] || '';
+      this.noResultsFound = params['noResultsFound'] === 'true';
+      console.log('Search query:', this.searchQuery);
+    });
+  }
+
+  fruitFilter(fruitQuery: string) {
+
+  }
+
+  vegetableFilter(vegQuery: string) {
+
+  }
+
+  alcoholFilter(alcoholQuery: string) {
+    
+  }
 }
