9e360388a1b71f24c538f3b94c52770eb9bc0775
Nico Linux Desktop <NicoloRomito@protonmail.com>
2025-04-09 14:57:06 +0200

    [UPDATE]: Add profile button when logged in, still need to be linked to the backend for a certain response. [TODO]: Create the Home button. [TODO]: Create the theme button on the top right. [TODO]: Look at the TODO already in the code.

--- projects/shared/src/lib/components/profile-button/profile-button.component.ts
+++ projects/shared/src/lib/components/profile-button/profile-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, effect } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,14 +7,18 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { PasswordModule } from 'primeng/password';
 import { DividerModule } from 'primeng/divider';
 import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
+import { SplitButton } from 'primeng/splitbutton';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormsModule, FormBuilder } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-profile-button',
   imports: [ButtonModule, Dialog, InputTextModule, AvatarModule,
             CheckboxModule, ReactiveFormsModule, FormsModule,
-            PasswordModule, DividerModule, ToastModule],
+            PasswordModule, DividerModule, ToastModule, NgIf, SplitButton],
   standalone: true,
   templateUrl: './profile-button.component.html',
   styleUrl: './profile-button.component.scss',
@@ -38,18 +42,82 @@ export class ProfileButtonComponent implements OnInit {
   ];
 
     // Booleans
-  loginVisible: boolean = false;
-  registerVisible: boolean = false;
+  visibles: { [key: string]: boolean } = {
+    isLoggedIn: false,
+    loginDialogVisible: false,
+    registerDialogVisible: false,
+  };
+
+  items: MenuItem[];
 
     // Form groups
   privacyForm!: FormGroup;
   credentialsForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private messageService: MessageService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private authService: AuthService,
+    private router: Router)
+  {
     this.credentialsForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
     })
+    // * PROFILE BUTTON LIST
+    this.items = [
+      {
+        label: 'Profile',
+        icon: 'pi pi-fw pi-user',
+        url: '/profile',
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-fw pi-cog',
+        url: '/profile/settings',
+        // TODO: learn how to add settings to the url from a button on the profile page
+        //   this.router.navigate(['/profile/settings']);
+      },
+      {
+        label: 'Privacy',
+        icon: 'pi pi-fw pi-shield',
+        command: () => {
+          // TODO: add a privacy policy dialog when this is clicked.
+          // showPrivacyPolicy();
+        }
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-fw pi-sign-out',
+        command: () => {
+          this.authService.logout()
+            .subscribe({
+              next: (response: any) => {
+                if (response) {
+                  this.messageService.add({
+                    severity: 'success',
+                    summary: 'Logout successful',
+                    detail: 'You have been logged out',
+                    life: 3000,
+                    closable: true,
+                  });
+                }
+              },
+              error: (error: any) => {
+                console.error('Logout failed', error);
+                this.messageService.add({
+                  severity: 'error',
+                  summary: 'Logout failed',
+                  detail: 'Please try again later',
+                  life: 5000,
+                  closable: true,
+                });
+              }
+            })
+          this.visibles['isLoggedIn'] = false;
+        }
+      }
+    ];
   }
 
   ngOnInit(): void {
@@ -63,20 +131,21 @@ export class ProfileButtonComponent implements OnInit {
   }
 
   setLoginVisible() {
-    this.loginVisible = !this.loginVisible;
+    this.visibles['loginDialogVisible'] = !this.visibles['loginDialogVisible'];
+    if (this.visibles['loginDialogVisible']) {
+      this.credentialsForm.reset();
+      this.privacyForm.reset({ privacyCheck: false });
+    }
+    console.log('Login form is visible', this.visibles['loginDialogVisible']);
   }
 
   // * This function is used to show the register form
   setRegisterVisible() {
-    this.registerVisible = !this.registerVisible;
-    if (this.registerVisible) {
+    this.visibles['registerDialogVisible'] = !this.visibles['registerDialogVisible'];
+    if (this.visibles['registerDialogVisible']) {
       this.credentialsForm.reset();
       this.privacyForm.reset({ privacyCheck: false });
     }
-  }
-
-  sendLoginData() {
-    console.log('Login data sent');
   }
 
   showError() {
@@ -89,16 +158,87 @@ export class ProfileButtonComponent implements OnInit {
     });
   }
 
-  sendRegisterData() {
-    if (this.privacyForm.valid) {
-      if (this.credentialsForm.invalid) {
-        console.error('Please fill in all the fields');
-        this.showError();
-        return;
-      }
-      console.log('Data sent', this.credentialsForm.controls['email'].value, this.credentialsForm.controls['password'].value);
+  checkForEmptyFields(): boolean {
+    if (this.credentialsForm.invalid) {
+      console.error('Please fill in all the fields');
+      this.showError();
+      return true;
     }
+    return false;
+  }
 
+  // * LOGIN
+  sendLoginData() {
+    if (this.checkForEmptyFields())
+      return;
+    console.log('Login data sent', this.credentialsForm.controls['email'].value, this.credentialsForm.controls['password'].value);
+    this.authService.login(
+      this.credentialsForm.controls['email'].value,
+      this.credentialsForm.controls['password'].value)
+      .subscribe({
+        next: (response: any) => {
+          if (response) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Login successful',
+              detail: 'You can now access your profile',
+              life: 3000,
+              closable: true,
+            });
+          }
+          this.visibles['isLoggedIn'] = true;
+        },
+        error: (error: any) => {
+          console.error('Login failed', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Login failed',
+            detail: 'Please try again later',
+            life: 5000,
+            closable: true,
+          });
+        }
+      });
+    this.visibles['isLoggedIn'] = true;
+    this.setLoginVisible();
+  }
+
+
+  // * REGISTRATION
+  sendRegisterData() {
+    if (this.checkForEmptyFields())
+      return;
+    console.log('Register data sent', this.credentialsForm.controls['email'].value, this.credentialsForm.controls['password'].value);
+    // ! to comment for profile button check
+    this.authService.register(
+      this.credentialsForm.controls['email'].value,
+      this.credentialsForm.controls['password'].value)
+      .subscribe({
+        next: (response: any) => {
+          if (response) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Registration successful',
+              detail: 'You can now access your profile',
+              life: 3000,
+              closable: true,
+            });
+          }
+          this.visibles['isLoggedIn'] = true;
+        },
+        error: (error: any) => {
+          console.error('Registration failed', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Registration failed',
+            detail: 'Please try again later',
+            life: 5000,
+            closable: true,
+          });
+        }
+
+      })
+    this.visibles['isLoggedIn'] = true;
     this.setRegisterVisible();
   }
 }
--- projects/shared/src/lib/services/auth.service.ts
+++ projects/shared/src/lib/services/auth.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // TODO: Add the backend URL to the environment file
+  private backendUrl: string = 'http://localhost:3000/api/auth';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.backendUrl}/login`, { email, password });
+  }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.backendUrl}/register`, { email, password });
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.backendUrl}/logout`, {});
+  }
+
+  getUserProfileData(): Observable<any> {
+    return this.http.get(`${this.backendUrl}/profile`);
+  }
+
+
 }
 
 // TODO: Add a method to check if the user is logged in
